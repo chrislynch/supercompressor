80ec8e7ded12df82f52b88a7e73c8a5f96b6871b
Chris Lynch <github@planetofthepenguins.com>
2012-04-27 10:12:00 +0100

    Made sections and menus better

--- actions/menu.php
+++ actions/menu.php
@@ -8,32 +8,25 @@ function action_menu_go(&$data){
     
     // Make an empty menu array, as we may ask for it even if there are no menus.
     $data['menu'] = array();
-    $data['menu']['Products'] = array();
-    $data['menu']['Brands'] = array();
+    $menus = array('Products'  => 'Category.Product',
+                    'Brands'   => 'Category.Brand',
+                    'Sections' => 'Section');
     
-    // Start loading up menus.
-    $menusSQL = 'SELECT value as category,count(value) as members
+    foreach($menus as $menuKey => $menuField){
+        
+        $data['menu'][$menuKey] = array();
+        
+        $menusSQL = 'SELECT value as category,count(value) as members
                  FROM   sc_data 
-                 WHERE  field = "Category.Product"
+                 WHERE  field = "' . $menuField . '"
                  GROUP BY value
                  ORDER BY value';
-    $menusData = mysql_query($menusSQL,$db);
-       
-    while($menuItem = mysql_fetch_assoc($menusData)){
-        $data['menu']['Products'][] = array('link'=>'?action=search&search.Category.Product=' . $menuItem['category'],
-                                            'text'=> $menuItem['category'] . ' (' . $menuItem['members'] . ')');
-    }
-    
-    $menusSQL = 'SELECT value as category,count(value) as members
-                 FROM   sc_data 
-                 WHERE  field = "Category.Brand"
-                 GROUP BY value
-                 ORDER BY value';
-    $menusData = mysql_query($menusSQL,$db);
-       
-    while($menuItem = mysql_fetch_assoc($menusData)){
-        $data['menu']['Brands'][] = array('link'=>'?action=search&search.Category.Brand=' . $menuItem['category'],
-                                            'text'=> $menuItem['category'] . ' (' . $menuItem['members'] . ')');
+        $menusData = mysql_query($menusSQL,$db);
+        
+        while($menuItem = mysql_fetch_assoc($menusData)){
+            $data['menu'][$menuKey][] = array('link'=>'?action=search&search.Category.Product=' . $menuItem['category'],
+                                                'text'=> $menuItem['category'] . ' (' . $menuItem['members'] . ')');
+        }
     }
     
 }
