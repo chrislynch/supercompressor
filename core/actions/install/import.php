fbc210368d87984b47846844c187c9c7fe1ac3f9
Chris Lynch <github@planetofthepenguins.com>
2012-04-30 17:26:25 +0100

    Little tweaks after import

--- core/actions/install/import.php
+++ core/actions/install/import.php
@@ -4,23 +4,36 @@
  */
 
 function action_install_import_go(&$data){
+    global $db;
     
     cliPrint('Import Started');
     
     $csvfile = fopen('import.csv','r');
+    $rowcount = -1;
+    while($csvrecord = fgetcsv($csvfile)){
+        $rowcount ++;
+    }
+    rewind($csvfile);
 
     $fields = fgetcsv($csvfile);
     $i = 1;
+    $importedrowcount = 0;
     while($csvrecord = fgetcsv($csvfile)){
         $record = array();
         for ($index = 0; $index < sizeof($fields); $index++) {
             array_drill_set($fields[$index], $csvrecord[$index], $record);
         }
         data_save($record);
-        cliPrint('Saved record ID ' . $record['ID']);
+        $importedrowcount ++;
+        $importPC = round(($importedrowcount / $rowcount) * 100,2);
+        cliPrint('Saved record ID ' . $record['ID'] . ' Import is ' . $importPC . '% complete');
         $i++;
     }
 
+    cliPrint('Stripping field_ and _value from Field names in sc_data');
+    mysql_query('UPDATE sc_data SET Field = REPLACE(Field,"field_","Specification.")',$db);
+    mysql_query('UPDATE sc_data SET Field = REPLACE(Field,"_value","")',$db);
+    
     cliPrint('Import Complete');
 }
 
