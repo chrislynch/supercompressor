a1953fbaefc707433e3de048bf1c2edc6845f0fe
Chris Lynch <github@planetofthepenguins.com>
2012-05-14 16:37:06 +0100

    The basics of a faceted search

--- core/actions/menu.php
+++ core/actions/menu.php
@@ -8,18 +8,21 @@ function action_menu_go(&$data){
     
     // Make an empty menu array, as we may ask for it even if there are no menus.
     $data['menu'] = array();
+    $data['filter'] = array();
+    
     $menus = array('Products'  => 'Category.Product',
                     'Brands'   => 'Category.Brand',
                     'Sections' => 'Section');
     
+    // Build code for regular menus
     foreach($menus as $menuKey => $menuField){
         
         $data['menu'][$menuKey] = array();
         
         $menusSQL = 'SELECT value as category,count(value) as members
                      FROM   sc_data 
-                     WHERE  field = "' . $menuField . '"
-                     GROUP BY value ' ;
+                     WHERE  field = "' . $menuField . '"';
+        $menusSQL .= 'GROUP BY value ' ;
         if ($menuKey == 'Sections') {
             $menusSQL .= 'ORDER BY count(value) DESC';
         } else {
@@ -34,6 +37,34 @@ function action_menu_go(&$data){
         }
     }
     
+    if ($data['actions'][0] == 'search') {
+        // We ALSO need to produce filters, to run ahead of our browses
+        $IDs = array();
+        foreach($data['_content'] as $key=>$value){
+            $IDs[] = $key;
+        }
+        $IDs = implode(',',$IDs);
+        
+        foreach($menus as $menuKey => $menuField){
+            if ($menuKey !== 'Sections'){
+                $data['filter'][$menuKey] = array();
+            }
+            
+            $menusSQL = 'SELECT value as category,count(value) as members
+                         FROM   sc_data 
+                         WHERE  field = "' . $menuField . '"';
+            $menusSQL .= ' AND ID IN (' . $IDs . ') ';
+            $menusSQL .= ' GROUP BY value ' ;
+            $menusSQL .= ' ORDER BY value';
+            
+            $menusData = mysql_query($menusSQL,$db);
+
+            while($menuItem = mysql_fetch_assoc($menusData)){
+                $data['filter'][$menuKey][] = array('link'=>'?action=search&search.' . $menuField . '=' . $menuItem['category'],
+                                                    'text'=> $menuItem['category'] . ' (' . $menuItem['members'] . ')');
+            }
+        }
+    }
 }
 
 ?>
--- core/widgets/categories.php
+++ core/widgets/categories.php
@@ -8,7 +8,12 @@ function widget_categories_go($widget_field,$widget_params,&$data){
     
     $return = '';
     foreach($data['_configuration']['categories'] as $category=>$categorySettings){
-        $return .= '<h3>' . $categorySettings['heading'] . '</h3>';
+        $return .= '<h3>Filter By ' . $categorySettings['heading'] . '</h3>';
+        $return .= '<ul>@@list.filter.' . $categorySettings['menu'] . '?li-class=menu-item@@</ul>';
+    }
+    
+    foreach($data['_configuration']['categories'] as $category=>$categorySettings){
+        $return .= '<h3>Browse By ' . $categorySettings['heading'] . '</h3>';
         $return .= '<ul>@@list.menu.' . $categorySettings['menu'] . '?li-class=menu-item@@</ul>';
     }
     return $return;
