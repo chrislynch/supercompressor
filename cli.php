2319b27d60dcbff61d124f993c3c4df4941b61e3
Chris Lynch <github@planetofthepenguins.com>
2012-05-15 16:43:03 +0100

    Extended cli.php to cover different domains

--- cli.php
+++ cli.php
@@ -1,7 +1,12 @@
 <?php
 
-include 'bootstrap.php';
+print_r($argv);
 
+if (isset($argv[1])){
+    $_SERVER['HTTP_HOST'] = $argv[1];
+}
+
+include 'bootstrap.php';
 include 'core/actions/install/import.php';
 
 action_install_import_go($data);
--- sites/www.solarpanelcentric.co.uk/config.php
+++ sites/www.solarpanelcentric.co.uk/config.php
@@ -1,5 +1,7 @@
 <?php
-array_drill_set('_configuration.site.name','SolarPanel',$data);
+$data['_configuration']['db']['schema']    = 'centric_solarpanel';
+
+array_drill_set('_configuration.site.name','SolarPanel<span class="centric">Centric</span>',$data);
 array_drill_set('_configuration.site.strapline','Bringing trade price solar panels to UK consumers',$data);
 
 array_drill_set('seo.title','solarpanelcentric',$data);
