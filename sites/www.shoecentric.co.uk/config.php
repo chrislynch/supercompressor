cc8e87da75def31be345160ee09bcbe67b89ca05
Chris Lynch <github@planetofthepenguins.com>
2012-05-16 23:05:23 +0100

    Moved import files around. Nothing special.

---
+++ sites/www.shoecentric.co.uk/config.php
@@ -0,0 +1,10 @@
+<?php
+$data['_configuration']['db']['schema']    = 'centric_shoe';
+
+array_drill_set('_configuration.site.name','Shoe<span class="centric">Centric</span>',$data);
+array_drill_set('_configuration.site.strapline','Design shoes, discount prices',$data);
+
+array_drill_set('seo.title','ShoeCentric',$data);
+
+
+?>
