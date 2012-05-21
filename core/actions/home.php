e9fe9a4ecca378766b4a5841867e00c9de21544e
Chris Lynch <github@planetofthepenguins.com>
2012-05-21 16:24:26 +0100

    Slightly reworked how home works. Needs a bit more work to make it truly data driven, I think.

--- core/actions/home.php
+++ core/actions/home.php
@@ -10,13 +10,13 @@ function action_home_go(&$data){
                             FROM sc_index sci
                             JOIN sc_data sd1 ON sd1.ID = sci.ID AND sd1.`Field` = "Product.SellPrice"
                             JOIN sc_data sd2 ON sd2.ID = sci.ID AND sd2.`Field` = "Product.ListPrice"
-                            ORDER BY (sd2.`Value` - sd1.`Value`) DESC
-                            LIMIT 4');
-    $data['_content']['sale'] = array();
+                            ORDER BY (sd2.`Value` - sd1.`Value`) DESC');
+    $data['_content']['home'] = array();
     while($record = mysql_fetch_assoc($records)){
-        data_load($record['ID'],$data['_content']['sale']);
+        data_load($record['ID'],$data['_content']['home']);
     }
     
+    /*
     $records = mysql_query('SELECT sci.ID
                             FROM sc_index sci
                             JOIN sc_data sd1 ON sd1.ID = sci.ID AND sd1.`Field` = "Product.SellPrice"
@@ -26,6 +26,7 @@ function action_home_go(&$data){
     while($record = mysql_fetch_assoc($records)){
         data_load($record['ID'],$data['_content']['budget']);
     }
+     */
     
 }
 
