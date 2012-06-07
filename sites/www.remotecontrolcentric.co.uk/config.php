c7dd67dec22b18956fdbc0225f1968de08ad9ea3
Chris Lynch <github@planetofthepenguins.com>
2012-06-07 12:36:24 +0100

    Prepping for next test upload. Fixed some bugs in Kirby pages

--- sites/www.remotecontrolcentric.co.uk/config.php
+++ sites/www.remotecontrolcentric.co.uk/config.php
@@ -1,6 +1,9 @@
 <?php
 if (!strstr($_SERVER['HTTP_HOST'],'dev.')){
     $data['_configuration']['db']['password']  = 'spider20';
+    
+    array_drill_set('seo.google.analytics.account','UA-31872896-2',$data);
+    array_drill_set('seo.google.webmastertools.account','Shk8GVA_8ltC7meV9luOBRbh_19ZfeWgNVdl6H2q6_w',$data);
 } 
 $data['_configuration']['db']['schema']    = 'centric_remotecontrol';
 
@@ -9,8 +12,7 @@ array_drill_set('_configuration.site.strapline','Replacing the UK\'s lost and da
 
 
 array_drill_set('seo.title','Remote Control Centric',$data);
-array_drill_set('seo.google.analytics.account','UA-31872896-2',$data);
-array_drill_set('seo.google.webmastertools.account','Shk8GVA_8ltC7meV9luOBRbh_19ZfeWgNVdl6H2q6_w',$data);
+
 
 
 array_drill_set('_configuration.about.welcome','
--- sites/www.solarpanelcentric.co.uk/config.php
+++ sites/www.solarpanelcentric.co.uk/config.php
@@ -1,6 +1,9 @@
 <?php
 if (!strstr($_SERVER['HTTP_HOST'],'dev.')){
     $data['_configuration']['db']['password']  = 'spider20';
+    
+    array_drill_set('seo.google.analytics.account','UA-31872896-1',$data);
+    array_drill_set('seo.google.webmastertools.account','fxi3B2YNo2XXO1-SMgTa1_Jt0vs7BkG9VZoBHtgYKoE',$data);
 }
 $data['_configuration']['db']['schema']    = 'centric_solarpanel';
 
@@ -10,15 +13,4 @@ array_drill_set('_configuration.site.strapline','Bringing trade price solar pane
 array_drill_set('seo.title','Solar Panel Centric - Centred on Solar Panels',$data);
 array_drill_set('seo.keywords','solar, solar panels, solar power',$data);
 array_drill_set('seo.description','Solar Panel Centric are dedicated to bringing solar panels to UK consumers at trade prices.',$data);
-
-array_drill_set('seo.google.analytics.account','UA-31872896-1',$data);
-array_drill_set('seo.google.webmastertools.account','fxi3B2YNo2XXO1-SMgTa1_Jt0vs7BkG9VZoBHtgYKoE',$data);
-
-array_drill_set('_configuration.about.welcome','
-',$data);
-
-array_drill_set('_configuration.about.short','',$data);
-
-array_drill_set('_configuration.about.long','
-',$data);
 ?>
